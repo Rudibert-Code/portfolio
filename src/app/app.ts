f34658e894d0d52deaaa887561360a264c30168b
Bjoern <onoofelogmail.com>
2026-05-21 15:12:07 +0200

    - imported skill-icons - created "my-skills" html - added skill aray - added for loop to render skills

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
 import { WhyMe } from './layout/why-me/why-me'
+import { MySkills } from './my-skills/my-skills';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,Footer,WhyMe],
+  imports: [RouterOutlet,Header,Footer,WhyMe,MySkills],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/my-skills/my-skills.ts
+++ src/app/my-skills/my-skills.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
 
+interface Skill {
+  img:string,
+  name:string,
+}
+
 @Component({
   selector: 'app-my-skills',
   imports: [],
   templateUrl: './my-skills.html',
   styleUrl: './my-skills.scss',
 })
-export class MySkills {}
+export class MySkills {
+  skills:Skill[]=[
+    {
+      img:'/assets/icons/Icons_Angular.png',
+      name:'Angular',
+    },
+    {
+      img:'/assets/icons/Icons_TypeScript.png',
+      name:'TypeScript',
+    },
+    {
+      img:'/assets/icons/Icons_JavaScript.png',
+      name:'JavaScript',
+    },
+    {
+      img:'/assets/icons/Icons_HTML.png',
+      name:'HTML',
+    },
+    {
+      img:'/assets/icons/Icons_CSS.png',
+      name:'CSS',
+    },
+    {
+      img:'/assets/icons/Icons_Git.png',
+      name:'Git',
+    },
+  ]
+}
