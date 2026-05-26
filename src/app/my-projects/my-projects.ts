2810094ad2470d6902279456ce2dcc964a998cec
Bjoern <onoofelogmail.com>
2026-05-26 12:07:20 +0200

    - added background to my-skills title
    - inported my-skills highlights

--- src/app/my-projects/my-projects.ts
+++ src/app/my-projects/my-projects.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
 
+interface Projects {
+  title:string,
+  selected:boolean,
+  about:string,
+  organisation?:string,
+  experiences?:string,
+}
+
 @Component({
   selector: 'app-my-projects',
   imports: [],
   templateUrl: './my-projects.html',
   styleUrl: './my-projects.scss',
 })
-export class MyProjects {}
+export class MyProjects {
+  projects:Projects[]=[
+    {
+      title:'1. Jump N Run',
+      selected:true,
+      about:'Project description...'
+    },
+    {
+      title:'2. Pokedex',
+      selected:false,
+      about:'Project description...',
+      organisation:'Workflow details...',
+      experiences:'Experiecnes...',
+    },
+  ]
+}
