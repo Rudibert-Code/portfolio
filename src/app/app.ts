c21bf5b934ae46de82156265240e2f4ef85c6c89
Bjoern <onoofelogmail.com>
2026-05-27 16:29:03 +0200

    - created main-footer.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,10 +7,11 @@ import { MySkills } from './my-skills/my-skills';
 import { MyProjects } from './my-projects/my-projects';
 import { Comments } from './comments/comments';
 import { ContactMe } from './contact-me/contact-me';
+import { MainFooter } from './main-footer/main-footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,Footer,WhyMe,MySkills,MyProjects,Comments,ContactMe],
+  imports: [RouterOutlet,Header,Footer,WhyMe,MySkills,MyProjects,Comments,ContactMe,MainFooter],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
