a0c515b56fda2e0eb862ea39d2c079980f3c61e7
Bjoern <onoofelogmail.com>
2026-06-01 16:37:42 +0200

    "contact me" added function to toggle button, when all textboxes & checkbox are active

--- src/app/contact-me/contact-me.ts
+++ src/app/contact-me/contact-me.ts
@@ -44,6 +44,7 @@ export class ContactMe {
       this.btnCondition = '/assets/checkbox/checkbox-default.png';
       boxChecked = false;
     }
+    this.toggleButton();
   }
 
   checkInputValue(id:string){
@@ -52,6 +53,9 @@ export class ContactMe {
   if (id == 'contactname' && section.value.length >= 4 && nameChecked == false) {
     document.getElementById('name')?.classList.remove('hide');
     document.getElementById('contactname')?.classList.remove('error');
+    document.getElementById('nameValue')?.classList.remove('error');
+    let changeName = document.getElementById('nameValue') as HTMLInputElement;
+    changeName.innerHTML = "Your Name";
     nameChecked = true;
   } else if(id == 'contactname' && section.value.length <= 3 && nameChecked == true){
     document.getElementById('name')?.classList.add('hide');
@@ -60,6 +64,9 @@ export class ContactMe {
   if (id == 'contactemail' && section.value.length >= 4 && emailChecked == false) {
     document.getElementById('email')?.classList.remove('hide');
     document.getElementById('contactemail')?.classList.remove('error');
+    document.getElementById('emailValue')?.classList.remove('error');
+    let changeName = document.getElementById('emailValue') as HTMLInputElement;
+    changeName.innerHTML = "Your Email";
     emailChecked = true;
   } else if(id == 'contactemail' && section.value.length <= 3 && emailChecked == true){
     document.getElementById('email')?.classList.add('hide');
@@ -68,19 +75,15 @@ export class ContactMe {
   if (id == 'contactmessage' && section.value.length >= 4 && messageChecked == false) {
     document.getElementById('message')?.classList.remove('hide');
     document.getElementById('contactmessage')?.classList.remove('error');
+    document.getElementById('messageValue')?.classList.remove('error');
+    let changeName = document.getElementById('messageValue') as HTMLInputElement;
+    changeName.innerHTML = "Your Message";
     messageChecked = true;
   } else if(id == 'contactmessage' && section.value.length <= 3 && messageChecked == true){
     document.getElementById('message')?.classList.add('hide');
     messageChecked = false;
   }
-
-  if (nameChecked == true && emailChecked == true && messageChecked == true) {
-    document.getElementById('btn-send')?.classList.remove('button-inactive');
-    document.getElementById('btn-send')?.classList.add('button-active');
-  } else{
-    document.getElementById('btn-send')?.classList.remove('button-active');
-    document.getElementById('btn-send')?.classList.add('button-inactive');
-  }
+  this.toggleButton();
  }
 
   sendMessage(){
@@ -89,16 +92,35 @@ export class ContactMe {
     }
     if(nameChecked == false){
       document.getElementById('contactname')?.classList.add('error');
+      document.getElementById('nameValue')?.classList.add('error');
+      let changeName = document.getElementById('nameValue') as HTMLInputElement;
+      changeName.innerHTML = "Your name is required";
     }
     if(emailChecked == false){
       document.getElementById('contactemail')?.classList.add('error');
+      document.getElementById('emailValue')?.classList.add('error');
+      let changeEMail = document.getElementById('emailValue') as HTMLInputElement;
+      changeEMail.innerHTML = "Your email is required";
     }
     if(messageChecked == false){
       document.getElementById('contactmessage')?.classList.add('error');
+      document.getElementById('messageValue')?.classList.add('error');
+      let changeMessage = document.getElementById('messageValue') as HTMLInputElement;
+      changeMessage.innerHTML = "Your message is required";
     }
     if(boxChecked == false){
       document.getElementById('contactCheckboxError')?.classList.remove('contact-checkbox');
       this.btnCondition = '/assets/checkbox/checkbox-error.png';
+    }
+  }
+
+  toggleButton(){
+    if (nameChecked == true && emailChecked == true && messageChecked == true && boxChecked == true) {
+      document.getElementById('btn-send')?.classList.remove('button-inactive');
+      document.getElementById('btn-send')?.classList.add('button-active');
+    } else{
+      document.getElementById('btn-send')?.classList.remove('button-active');
+      document.getElementById('btn-send')?.classList.add('button-inactive');
     }
   }
 }
