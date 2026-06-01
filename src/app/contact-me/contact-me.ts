4655e9c0ddf59d15181a433a5ed4399847a13440
Bjoern <onoofelogmail.com>
2026-06-01 16:22:31 +0200

    "contact me" - highlight text-boxes, if send button is pressed without all checks - activate button when all  checks a re met

--- src/app/contact-me/contact-me.ts
+++ src/app/contact-me/contact-me.ts
@@ -8,7 +8,10 @@ interface Contacts{
   highlight:boolean
 }
 
-
+let nameChecked:boolean = false;
+let emailChecked:boolean = false;
+let messageChecked:boolean = false;
+let boxChecked:boolean = false;
 
 @Component({
   selector: 'app-contact-me',
@@ -34,10 +37,69 @@ export class ContactMe {
   ]
 
   checkBox(){
-    this.btnCondition = '/assets/checkbox/checkbox-checked.png';
+    if (boxChecked == false) {
+      this.btnCondition = '/assets/checkbox/checkbox-checked.png';
+      boxChecked = true;
+    } else{
+      this.btnCondition = '/assets/checkbox/checkbox-default.png';
+      boxChecked = false;
+    }
   }
 
-  contactSend(){
-    
+  checkInputValue(id:string){
+  let section = document.getElementById(id) as HTMLInputElement;
+
+  if (id == 'contactname' && section.value.length >= 4 && nameChecked == false) {
+    document.getElementById('name')?.classList.remove('hide');
+    document.getElementById('contactname')?.classList.remove('error');
+    nameChecked = true;
+  } else if(id == 'contactname' && section.value.length <= 3 && nameChecked == true){
+    document.getElementById('name')?.classList.add('hide');
+    nameChecked = false;
+  }
+  if (id == 'contactemail' && section.value.length >= 4 && emailChecked == false) {
+    document.getElementById('email')?.classList.remove('hide');
+    document.getElementById('contactemail')?.classList.remove('error');
+    emailChecked = true;
+  } else if(id == 'contactemail' && section.value.length <= 3 && emailChecked == true){
+    document.getElementById('email')?.classList.add('hide');
+    emailChecked = false;
+  }
+  if (id == 'contactmessage' && section.value.length >= 4 && messageChecked == false) {
+    document.getElementById('message')?.classList.remove('hide');
+    document.getElementById('contactmessage')?.classList.remove('error');
+    messageChecked = true;
+  } else if(id == 'contactmessage' && section.value.length <= 3 && messageChecked == true){
+    document.getElementById('message')?.classList.add('hide');
+    messageChecked = false;
+  }
+
+  if (nameChecked == true && emailChecked == true && messageChecked == true) {
+    document.getElementById('btn-send')?.classList.remove('button-inactive');
+    document.getElementById('btn-send')?.classList.add('button-active');
+  } else{
+    document.getElementById('btn-send')?.classList.remove('button-active');
+    document.getElementById('btn-send')?.classList.add('button-inactive');
+  }
+ }
+
+  sendMessage(){
+    if (nameChecked == true && emailChecked == true && messageChecked == true && boxChecked == true) {
+      console.log("Email gesendet");
+    }
+    if(nameChecked == false){
+      document.getElementById('contactname')?.classList.add('error');
+    }
+    if(emailChecked == false){
+      document.getElementById('contactemail')?.classList.add('error');
+    }
+    if(messageChecked == false){
+      document.getElementById('contactmessage')?.classList.add('error');
+    }
+    if(boxChecked == false){
+      document.getElementById('contactCheckboxError')?.classList.remove('contact-checkbox');
+      this.btnCondition = '/assets/checkbox/checkbox-error.png';
+    }
   }
 }
+
