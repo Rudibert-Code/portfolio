e0d06c5943900b81d3bd1d5655a7f851f79d0e02
Bjoern <onoofelogmail.com>
2026-06-02 17:32:14 +0200

    "My Projects" project selection logic WIP

--- src/app/my-projects/my-projects.ts
+++ src/app/my-projects/my-projects.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 interface Projects {
   title:string,
-  selected:boolean,
-  about:string,
-  organisation?:string,
-  experiences?:string,
+  projectID:number
 }
 interface Technologies {
   icon1:string,
@@ -19,14 +16,24 @@ interface Technologies {
     {
       title:'1. Jump N Run',
       selected:true,
-      about:'Project description...'
+      about:'A 2D platformer. No game engines used during this projects.',
+      organisation:'',
+      experiences:'I learned a lot.',
+      tech_1:'/assets/icons/Icons_JavaScript.png',
+      tech_2:'/assets/icons/Icons_HTML.png',
+      tech_3:'/assets/icons/Icons_CSS.png',
+      img:'/assets/projetcs/SpaceBlast.png'
     },
     {
-      title:'2. Pokedex',
+      title:'2. Pokédex',
       selected:false,
-      about:'Project description...',
-      organisation:'Workflow details...',
-      experiences:'Experiecnes...',
+      about:'A collection of the first 151 Pokémon, with optional detail viewer and search option.',
+      organisation:'',
+      experiences:'There are SO MANY pokémon!',
+      tech_1:'/assets/icons/Icons_JavaScript.png',
+      tech_2:'/assets/icons/Icons_HTML.png',
+      tech_3:'/assets/icons/Icons_CSS.png',
+      img:'/assets/projetcs/pokemon.jpg'
     },
   ]
 
@@ -38,26 +45,21 @@ interface Technologies {
   templateUrl: './my-projects.html',
   styleUrl: './my-projects.scss',
 })
+
 export class MyProjects {
+
   projects:Projects[]=[
     {
       title:'1. Jump N Run',
-      selected:true,
-      about:'Project description...'
+      projectID:1,
     },
     {
       title:'2. Pokedex',
-      selected:false,
-      about:'Project description...',
-      organisation:'Workflow details...',
-      experiences:'Experiecnes...',
+      projectID:2,
     },
-  ]
-  technologies:Technologies[]=[
-    {
-      icon1: '/assets/icons/Icons_JavaScript.png',
-      icon2: '/assets/icons/Icons_HTML.png',
-      icon3: '/assets/icons/Icons_CSS.png',
+      {
+      title:'3. WIP',
+      projectID:3,
     },
   ]
 
