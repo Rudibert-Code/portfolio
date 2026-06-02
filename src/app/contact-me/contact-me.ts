81925345e13e0746b30690e1b134884b340b008c
Bjoern <onoofelogmail.com>
2026-06-02 16:53:30 +0200

    "Contact Me" reworked Phone- & Email-Links

--- src/app/contact-me/contact-me.ts
+++ src/app/contact-me/contact-me.ts
@@ -2,12 +2,6 @@ import { Component } from '@angular/core';
 
 let checkbox:string = '/assets/checkbox/checkbox-default.png';
 
-interface Contacts{
-  icon:string,
-  info:string | number,
-  highlight:boolean
-}
-
 let nameChecked:boolean = false;
 let emailChecked:boolean = false;
 let messageChecked:boolean = false;
@@ -22,19 +16,6 @@ let boxChecked:boolean = false;
 export class ContactMe {
 
   btnCondition:string = '/assets/checkbox/checkbox-default.png';
-
-  contacts:Contacts[]=[
-    {
-      icon:'/assets/icons/Icons_email.png',
-      info:'E-Mail: onoofelo@gmail.com',
-      highlight:false
-    },
-    {
-      icon:'/assets/icons/Icons_phone.png',
-      info:'Tel: 01707821309',
-      highlight:false
-    },
-  ]
 
   checkBox(){
     if (boxChecked == false) {
