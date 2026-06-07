c05a30971001f8628979346c1d7bf58a700a2cd8
Bjoern <onoofelogmail.com>
2026-06-07 20:01:21 +0200

    - created legal-notice text; - linked legal notice component to mein-page via routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainPage } from './main-page/main-page';
 import { PrivacyPolicy } from './privacy-policy/privacy-policy';
+import { LegalNotice } from './legal-notice/legal-notice';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path:'policy',
         component:PrivacyPolicy
+    },
+    {
+        path:'legal',
+        component:LegalNotice
     },
 ];
---
+++ src/app/legal-notice/legal-notice.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NavBar } from '../nav-bar/nav-bar';
+
+@Component({
+  selector: 'app-legal-notice',
+  imports: [NavBar],
+  templateUrl: './legal-notice.html',
+  styleUrl: './legal-notice.scss',
+})
+export class LegalNotice {}
