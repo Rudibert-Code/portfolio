c225f705528bc8f31697dd04a322d9c1190a00d9
Bjoern <onoofelogmail.com>
2026-06-08 11:44:24 +0200

    adjusted footer container alignment for mobile

--- src/app/legal-notice/legal-notice.ts
+++ src/app/legal-notice/legal-notice.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { NavBar } from '../nav-bar/nav-bar';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [RouterLink],
+  imports: [RouterLink, NavBar],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss',
 })
--- src/app/privacy-policy/privacy-policy.ts
+++ src/app/privacy-policy/privacy-policy.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { NavBar } from '../nav-bar/nav-bar';
+
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [RouterLink],
+  imports: [RouterLink,NavBar],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.scss',
 })
